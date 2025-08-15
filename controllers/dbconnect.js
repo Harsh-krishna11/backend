e92d8bc067cce7667ef6280c381ad6092da74d40
Harsh-krishna11 <141502435+Harsh-krishna11@users.noreply.github.com>
2025-08-15 21:49:46 +0530

    code backend

---
+++ controllers/dbconnect.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  const DB_NAME = "lazydata";
+  try {
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MONGODB_URI}/${DB_NAME}`,
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    );
+    console.log(
+      `MongoDB connected! Host: ${connectionInstance.connection.host}`
+    );
+  } catch (err) {
+    console.error("MongoDB connection error:", err);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
